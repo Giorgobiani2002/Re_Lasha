d060f7826e6df1a3ba36af3b78d435e859dbca8d
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2024-06-12 15:14:50 +0400

    new feature/slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
-AOS.init(
-    
-);
+AOS.init();
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+const swiper2 = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    583: {
+      slidesPerView: 2,
+      
+    },
+    // when window width is >= 480px
+    863: {
+      slidesPerView: 3,
+      
+    },
+    // when window width is >= 640px
+    1440: {
+      slidesPerView: 4,
+      
+    }
+  }
+})
